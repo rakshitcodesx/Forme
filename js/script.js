075b530287296b494237489eeedf5ee8080de028
rakshitcodesx <rakshitop100@gmail.com>
2026-06-02 13:50:09 +0530

    Initial commit - homepage and signup page

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const getStartedBtn =
+document.getElementById("getStartedBtn");
+
+const loginBtn =
+document.getElementById("loginBtn");
+
+getStartedBtn.addEventListener("click", () => {
+
+    alert("Move to Sign Up Page");
+
+});
+
+loginBtn.addEventListener("click", () => {
+
+    alert("Move to Login Page");
+
+});
---
+++ js/signup.js
@@ -0,0 +1,51 @@
+const password =
+document.getElementById("password");
+
+const confirmPassword =
+document.getElementById("confirmPassword");
+
+const togglePassword =
+document.getElementById("togglePassword");
+
+const toggleConfirm =
+document.getElementById("toggleConfirm");
+
+togglePassword.addEventListener("click",()=>{
+
+    password.type =
+    password.type === "password"
+    ? "text"
+    : "password";
+
+});
+
+toggleConfirm.addEventListener("click",()=>{
+
+    confirmPassword.type =
+    confirmPassword.type === "password"
+    ? "text"
+    : "password";
+
+});
+
+document
+.getElementById("signupForm")
+.addEventListener("submit",(e)=>{
+
+    e.preventDefault();
+
+    if(
+        password.value !==
+        confirmPassword.value
+    ){
+
+        alert("Passwords do not match");
+
+        return;
+    }
+
+    alert(
+        "Account Created Successfully"
+    );
+
+});
